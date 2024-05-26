17a393352e8a410663f4ed76a90c642fe49b3a4d
Yuki-Nakamura4 <Yuki.Nakamura4@jp.ricoh.com>
2024-05-26 20:58:54 +0900

    とりあえずのゲーム完成

--- src/app/result/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Result() {
-  return <div>結果発表</div>;
-}
--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,7 +1,53 @@
-// ダミーデータの例
+// ダミーデータ
 export const problems = [
-  { problem: "こんにちは", answer: "konnichiha" },
-  { problem: "さようなら", answer: "sayounara" },
-  { problem: "ありがとう", answer: "arigatou" },
-  // 他の問題を追加
+  {
+    problem: "メルト 溶けてしまいそう",
+    answer: "merutotoketesimaisou",
+    title: "メルト",
+  },
+  {
+    problem: "あなたと私でランデブー？",
+    answer: "anatatowatasiderandebu-?",
+    title: "マトリョシカ",
+  },
+  {
+    problem: "私は今日も転がります",
+    answer: "watasihakyoumokorogarimasu",
+    title: "ローリンガール",
+  },
+  {
+    problem: "国語がどうもダメで嫌いでした",
+    answer: "kokugogadoumodamedekiraidesita",
+    title: "ロストワンの号哭",
+  },
+  {
+    problem: "愛を謳って謳って雲の上",
+    answer: "aiwoutatteutattekumonoue",
+    title: "シャルル",
+  },
+  {
+    problem: "ああ 化石になっちまうよ",
+    answer: "aakasekininattimauyo",
+    title: "きゅうくらりん",
+  },
+  {
+    problem: "案外そんなフューチャー",
+    answer: "angaisonnafyu-tya-",
+    title: "メルティランドナイトメア",
+  },
+  {
+    problem: "ハイハイチャイナ ちょちょ夢心地",
+    answer: "haihaityainatyotyoyumegokoti",
+    title: "いーあるふぁんくらぶ",
+  },
+  {
+    problem: "空へ舞う世界の彼方",
+    answer: "sorahemausekainokanata",
+    title: "アスノヨゾラ哨戒班",
+  },
+  {
+    problem: "淡い空 明けの蛍",
+    answer: "awaisoraakenohotaru",
+    title: "夜明けと蛍",
+  },
 ];
